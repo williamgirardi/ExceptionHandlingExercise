eb1d37f77b0359dbe066272c39a4a014a11d2ed2
William Girardi <williamsolongirardi@gmail.com>
2021-06-07 05:49:43 -0300

    Exception handling exercise

---
+++ ExceptionHandlingExercise/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExceptionHandlingExercise.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ ExceptionHandlingExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using ExceptionHandlingExercise.Entities;
+using System;
+using System.Globalization;
+using ExceptionHandlingExercise.Entities.Exceptions;
+
+namespace ExceptionHandlingExercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter account data ");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw Limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                acc.WithDraw(amount);
+                Console.Write("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
+        }
+    }
+}
